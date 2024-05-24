c48b93750630f2e43c7899b74c63036f436864b8
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-05-24 08:54:28 +0100

    first

---
+++ src/router/Router.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createBrowserRouter } from "react-router-dom";
+import { lazy } from "react";
+import HomeLayout from "../layout/HomeLayout";
+// import HomeComp from "../pages/HomePage/HomeComp";
+
+
+const HomeComp = lazy(() => import("../pages/HomePage/HomeComp"))
+
+
+export const element = createBrowserRouter([
+    {
+        path:"/",
+        element: <HomeLayout />,
+        children: [
+            {
+                index: true,
+                element: <HomeComp />
+            }
+        ]
+    }
+])
+
