82aa7e1a165f2f92e357f3b9316e3a9ae3d1e4b1
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-07-03 11:57:51 +0100

    products

--- src/pages/HomePage/Products2.tsx
+++ src/pages/HomePage/Products2.tsx
@@ -3,15 +3,21 @@
 import { FaArrowRightLong } from "react-icons/fa6";
 import Products2Card from "../../components/static/Products2Card";
 import gen from "../../assets/images/electronics/gen3.png";
+import chair from "../../assets/images/electronics/4PCS-outdoor-Furniture-Set-Conversation-Glass-Table-Top-Cushioned-Sofa-Beige.jpeg";
 import headphone from "../../assets/images/electronics/headPhone3.jpeg";
+import door from "../../assets/images/electronics/Single-security-doors-modern-secure-door-sigma.jpg";
 import speaker from "../../assets/images/electronics/bluetooth-Speaker.jpg";
 import RevealMap from "../../animation/RevealMap";
-// import fan from "../../assets/images/electronics/fan.png";
-// import washing from "../../assets/images/electronics/washing.png";
-// import chan from "../../assets/images/electronics/chadelia.png";
-// import ps4 from "../../assets/images/electronics/ps4.png";
-// import toast from "../../assets/images/electronics/toast.png";
-// import light from "../../assets/images/electronics/light.png";
+import fan from "../../assets/images/electronics/ox-18-fan-adjustable-standing-black.jpg";
+import washing from "../../assets/images/electronics/hisense-5kg-topLoadTwinTub-washingMachine.jpg";
+import chan from "../../assets/images/electronics/modern-13-light-chandeliar-hom-decor.jpg";
+import tv from "../../assets/images/electronics/43-HISENSE-4k-smartLed-tv.jpg";
+import wire from "../../assets/images/electronics/electric-wire-and-cables.jpg";
+import fridge from "../../assets/images/electronics/gh-050122-best-smart-refrigerators.png";
+import ornament from "../../assets/images/electronics/copper-gold-coral-ornaments-homeAccessories-mableCraft.jpg";
+import toast from "../../assets/images/electronics/binatone-4SliceToaster-pop414.jpg";
+import ornament2 from "../../assets/images/electronics/Nordic-Ornaments-Decorative-Design-Table-Decoration-Accessories-for-Home-Modern.webp";
+import tv2 from "../../assets/images/electronics/SLE-55US800TCSB-2-smartUHD-TV.jpg";
 
 
 
@@ -31,11 +37,107 @@ const Products2 = () => {
                 {/* <Slider {...settings}> */}
 
                     <Products2Card 
+                        image={tv2}
+                        title="Smart UHD TV"
+                        details1="SLE-55US800"
+                        details2="Home Appliances"
+                        details3=""
+                    />
+
+                    <Products2Card 
+                        image={chair}
+                        title="4-PCS Furniture"
+                        details1="Outdoor Conversation"
+                        details2="Glass Table"
+                        details3="Furnitures"
+                    />
+
+                    <Products2Card 
                         image={gen}
                         title="Firman Generator"
                         details1="10.0 kva"
                         details2="1 Year Warranty"
-                        details3="24 Hours Non-Stops"
+                        details3="Electrical"
+                    />
+
+                    <Products2Card 
+                        image={fan}
+                        title="OX Standing Fan"
+                        details1="16' "
+                        details2="Adjustable. Black."
+                        details3="Home Appliances"
+                    />
+
+                    <Products2Card 
+                        image={ornament}
+                        title="Coral Ornaments"
+                        details1="Copper Gold"
+                        details2="Mable Craft"
+                        details3="Home Accessories"
+                    />
+
+                   <Products2Card 
+                        image={washing}
+                        title="Hisense Washing Machine"
+                        details1="5kg Top-Load Loading"
+                        details2="Twin Tub"
+                        details3="Home Appliances"
+                    />
+
+                    <Products2Card 
+                        image={chan}
+                        title="Luxury Chandeliar"
+                        details1="13 Light"
+                        details2="Cystal LED"
+                        details3="Home Accesories"
+                     />
+
+                    <Products2Card 
+                        image={tv}
+                        title="Hisense 43'"
+                        details1="4k Smart LED"
+                        details2="Home Applainces"
+                        details3=""
+                    />
+
+                    <Products2Card 
+                        image={door}
+                        title="Security Door"
+                        details1="Modern Single Sigma"
+                        details2="Home Applainces"
+                        details3=""
+                    />
+
+                    <Products2Card 
+                        image={ornament2}
+                        title="Nordic Ornamnets"
+                        details1="Table Decor"
+                        details2="Home Applainces"
+                        details3=""
+                    />
+
+                    <Products2Card 
+                        image={fridge}
+                        title="Smart Refrigirator"
+                        details1="GH 050122"
+                        details2="Home Appliances"
+                        details3=""
+                    />
+
+                    <Products2Card 
+                        image={wire}
+                        title="Electric Wire & Cables"
+                        details1=""
+                        details2="Electricals"
+                        details3=""
+                    />
+
+                    <Products2Card 
+                        image={toast}
+                        title="Binatone Toaster"
+                        details1="4 Slice"
+                        details2="Pop 414"
+                        details3="Home Appliances"
                     />
 
                     <Products2Card 
@@ -53,54 +155,6 @@ const Products2 = () => {
                         details2="Wireless Bluetooth"
                         details3="12 Hours Battery Power"
                     />
-
-                    {/* <Products2Card 
-                        image={fan}
-                        title="Scanfrost AC Stand Fan"
-                        details1="16' AC"
-                        details2="Remote Enabled"
-                        details3="SFMF16RC"
-                    /> */}
-
-                   {/* <Products2Card 
-                        image={washing}
-                        title="Samsung Washing Machine"
-                        details1="6kg Front Loading"
-                        details2="Silver"
-                        details3=""
-                    /> */}
-
-                {/* //     <Products2Card  */}
-                {/* //         image={chan}
-                //         title="Luxury Chandeliar"
-                //         details1="Crystal LED"
-                //         details2="High Ceiling"
-                //         details3=""
-                //     /> */}
-
-                {/* //     <Products2Card 
-                //         image={ps4}
-                //         title="Sony PS4"
-                //         details1="Pro - 1TB"
-                //         details2="Console - Black"
-                //         details3=""
-                //     /> */}
-
-                {/* //    <Products2Card 
-                //         image={toast}
-                //         title="Breville Toastining Machine"
-                //         details1="4-Face Toast"
-                //         details2="Electric Enabled"
-                //         details3=""
-                //     /> */}
-
-                {/* //     <Products2Card 
-                //         image={light}
-                //         title="Interior Decor Light"
-                //         details1="4 Colored Light"
-                //         details2=""
-                //         details3=""
-                //     /> */}
 
 
                 {/* </Slider> */}
